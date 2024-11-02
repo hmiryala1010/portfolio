75de4df44b16cbb5999c8487306d013fed5c2a95
HASINI MIRYALA <160294217+hmiryala1010@users.noreply.github.com>
2024-11-02 16:25:40 -0500

    changed baseurl in api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5002',
+  baseURL: process.env.REACT_APP_API_URL || ,
 });
 
 export const fetchData = async () => {
